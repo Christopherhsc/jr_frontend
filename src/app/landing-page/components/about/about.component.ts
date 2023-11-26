657dbc645d3033a68bdc5d57c0ff17e85bfae90d
Christopher <christopher.hsc2520@gmail.com>
2023-11-27 00:49:29 +0100

    about pictures + content added + animations added

--- src/app/landing-page/components/about/about.component.ts
+++ src/app/landing-page/components/about/about.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { slideInAnimation } from '../../../shared/animations/animations'
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  animations: [slideInAnimation],
 })
 export class AboutComponent {
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {}
+
+  goToAnotherComponent() {
+    this.router.navigateByUrl('/other-component');
+  }
 }
